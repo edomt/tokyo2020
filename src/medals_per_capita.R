48aecabb4e99cf91ab3cba374586e04c7435aaf4
Edouard Mathieu <edmat@pm.me>
2021-08-06 16:06:30 +0200

    Add teams with 0 medal

--- src/medals_per_capita.R
+++ src/medals_per_capita.R
@@ -11,6 +11,15 @@ df <- read_html(url) %>%
   html_table() %>%
   data.table()
 
+noc_list <- read_html("https://olympics.com/tokyo-2020/olympic-games/en/results/all-sports/nocs-list.htm") %>%
+  html_nodes(".list-unstyled a") %>%
+  html_text() %>%
+  str_squish() %>%
+  setdiff(df$`Team/NOC`) %>%
+  setdiff("Refugee Olympic Team")
+
+df <- rbindlist(list(df, data.table(`Team/NOC` = noc_list)), fill = TRUE)
+
 mapping <- fread("input/country_mapping.csv")
 
 df[, entity := mapvalues(`Team/NOC`, mapping$from, mapping$to)]
@@ -20,6 +29,7 @@ pop <- fread("input/population.csv")
 df <- merge(df, pop, by = "entity", all.x = TRUE)
 stopifnot(all(!is.na(df$population)))
 
+df[is.na(Total), Total := 0]
 df[, medals_per_million := round(1e6 * Total / population, 4)]
 setorder(df, -medals_per_million)
 
